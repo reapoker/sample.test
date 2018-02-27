2be0247dbfd46d94b153af0333d6b0e0ed7825a4
reapoker <936771816@qq.com>
2018-02-27 04:41:00 +0000

    repair

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -99,8 +99,8 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'aufree@yousails.com';
-        $name = 'Aufree';
+        $from = '936771816@qq.com';
+        $name = 'reapoker';
         $to = $user->email;
         $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
